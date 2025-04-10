9aab483df59dae674f112a1c611d80d6f513f73c
michael_neidinger <michael_neidinger@PoisonedStarlight>
2025-04-10 14:54:08 -0500

    maven archetype creates bare minimum working discord bot

---
+++ src/main/resources/archetype-resources/src/main/java/__artifactId__Application.java
@@ -0,0 +1,13 @@
+package ${package};
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ${artifactId}Application {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(${artifactId}Application.class, args);
+	}
+
+}
