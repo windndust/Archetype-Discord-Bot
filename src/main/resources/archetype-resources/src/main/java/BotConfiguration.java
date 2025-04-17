9da5d880434eea2cc327c47fa4ee90ccde9705df
Michael Neidinger <michael.neidinger@gmail.com>
2025-04-16 22:28:41 -0500

    remove incorrect self-import in BotConfiguration

--- src/main/resources/archetype-resources/src/main/java/BotConfiguration.java
+++ src/main/resources/archetype-resources/src/main/java/BotConfiguration.java
@@ -2,7 +2,6 @@ package ${package};
 
 import java.util.List;
 
-import org.mneidinger.discordbot.BotConfiguration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
